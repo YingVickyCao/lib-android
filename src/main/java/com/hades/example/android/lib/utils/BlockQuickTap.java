16b50dda985837b55878d7fdc8fa134e30951953
YingVickyCao <ying.vicky.cao@outlook.com>
2022-05-25 19:18:58 +0800

    refactor

---
+++ src/main/java/com/hades/example/android/lib/utils/BlockQuickTap.java
@@ -0,0 +1,35 @@
+package com.hades.example.android.lib.utils;
+
+import androidx.annotation.IdRes;
+
+public class BlockQuickTap {
+
+    private static String lastBtnId = "";
+    private static long lastClickTsOfBtn;
+    public static long MIN_CLICK_DELAY_MS_OF_BTN = 200;
+
+    public static long MIN_TIME_INTERVAL_OF_TO_SHOW_DIALOG = 500;
+
+    public static boolean isUserFastOperation() {
+        return isUserFastOperation(-1, MIN_CLICK_DELAY_MS_OF_BTN);
+    }
+
+    public static boolean isUserFastOperation(@IdRes int btnId) {
+        return isUserFastOperation(btnId, MIN_CLICK_DELAY_MS_OF_BTN);
+    }
+
+    public static boolean isUserFastOperation(@IdRes int btnId, long clickDurationMs) {
+        return isUserFastOperation(String.valueOf(btnId), clickDurationMs);
+    }
+
+    public static boolean isUserFastOperation(String btnId, long clickDurationMs) {
+        long current = System.currentTimeMillis();
+        long duration = current - lastClickTsOfBtn;
+        if ((lastBtnId.equalsIgnoreCase(btnId)) && (0 < duration) && (duration < clickDurationMs)) {
+            return true;
+        }
+        lastBtnId = String.valueOf(btnId);
+        lastClickTsOfBtn = current;
+        return false;
+    }
+}
